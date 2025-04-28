788c7980d339a89efbb9f0d583a14cd7317c57a3
fer1421 <00154325@uca.edu.sv>
2025-04-28 15:27:13 -0600

    ejercicio labo 4

--- ejerciciolab4.cpp
+++ ejerciciolab4.cpp
@@ -8,10 +8,13 @@ int main()
 
     if(numero > 0){
         cout<<"su numero es positivo"<<endl;
+        //si el numero es mayor a 0, indicar que es positivo
     } else if (numero < 0){
         cout<<"su numero es negativo"<<endl;
+        //si el numero es menor a 0, indicar que es negativo
     } else if (numero == 0){
         cout<<"su numero es 0"<<endl;
+        //si el numero es 0, indicar que es 0
     } else {}
 
     return 0;
